19fa19111549d4e54c78df319a3282ece871cc10
bnguy118 <bnguy118@ucsc.edu>
2023-01-19 02:09:26 +0000

    updated more functionality for memory

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -21,15 +21,10 @@ int main(void) {
 
     // Variables to parse first line
     char *saveptr;
-    char *delimit = " \n";
 
     // Get Command
-    char *clStatement = strtok_r(buf, delimit, &saveptr);
+    char *clStatement = strtok_r(buf, " ", &saveptr);
     char *command = clStatement;
-
-    // Get Location
-    clStatement = strtok_r(NULL, delimit, &saveptr);
-    char *location = clStatement;
 
     // Make strings to check validity of commands
     char *get = "get";
@@ -37,17 +32,44 @@ int main(void) {
 
     // If command is get
     if (strcmp(command, get) == 0) {
+        // Check for newline to proceed
+        for(int i = 0; saveptr[i] != '\n'; i++) {
+            if(saveptr[i] == ' ' || saveptr[i] == '\0') {
+                fprintf(stderr, "Invalid Command\n");
+                return(EXIT_FAILURE);
+            }
+        }
+        // Get Location
+        clStatement = strtok_r(NULL, "\n", &saveptr);
+        char *location = clStatement;
+
+        // Check for invalid extra junk input
+        char *clStatement = strtok_r(NULL, "", &saveptr);
+        char *junk = clStatement;
+        if(junk != NULL) {
+            fprintf(stderr, "Invalid Command\n");
+            return(EXIT_FAILURE);
+        }
+
         // Open file for reading and check for validity
         int fd;
         if ((fd = open(location, O_RDONLY, 0444)) == -1) {
-            fprintf(stderr, "Invalid Filepath\n");
+            fprintf(stderr, "Invalid Command\n");
             return (EXIT_FAILURE);
         }
 
         // Write file contents to stdout
         int readBytes;
         while ((readBytes = read(fd, buf, sizeof(buf))) > 0) {
-            write(STDOUT_FILENO, buf, readBytes);
+            int writtenBytes, totalWritten = 0;
+            while (totalWritten < readBytes) {
+                writtenBytes = write(STDOUT_FILENO, buf + totalWritten, readBytes);
+                totalWritten += writtenBytes;
+            }
+        }
+        if(readBytes  == -1) {
+            fprintf(stderr, "Invalid Command\n");
+            return(EXIT_FAILURE);
         }
 
         // Close file and exitg
@@ -57,20 +79,44 @@ int main(void) {
 
     // If command is set
     else if (strcmp(command, set) == 0) {
+        for(int i = 0; clStatement[i] != '\n'; i++) {
+            if(clStatement[i] == ' ') {
+                fprintf(stderr, "Invalid Command\n");
+                return(EXIT_FAILURE);
+            }
+        }
+
+        // Get Location
+        clStatement = strtok_r(NULL, "\n", &saveptr);
+        char *location = clStatement;
+        
         // Open file for writing and check for validity
         int fd;
         if ((fd = open(location, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
-            fprintf(stderr, "Invalid Filepath\n");
+            fprintf(stderr, "Invalid Command\n");
             return (EXIT_FAILURE);
         }
 
-        // Write contents to buffer from initial read() call for first line
-        write(fd, saveptr, strlen(saveptr));
+        // Write contents to buffer from initial read() call for first line.
+        int writtenBytes = 0;
+        unsigned long totalWritten = 0;
+            while (totalWritten < strlen(saveptr)) {
+                writtenBytes = write(fd, saveptr + totalWritten, strlen(saveptr));
+                totalWritten += writtenBytes;
+            }
 
         // Continue to read if user would like to input more than initial input
         int readBytes;
         while ((readBytes = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-            write(fd, buf, readBytes);
+            int writtenBytes, totalWritten = 0;
+            while (totalWritten < readBytes) {
+                writtenBytes = write(fd, buf + totalWritten, readBytes);
+                totalWritten += writtenBytes;
+            }
+        }
+        if(readBytes  == -1) {
+            fprintf(stderr, "Invalid Command\n");
+            return(EXIT_FAILURE);
         }
 
         // Write OK\n to stdout
